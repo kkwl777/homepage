508000ce45fec90bb35b686e7e78f0c2b07533e0
kevoli7 <likev77@gmail.com>
2022-05-17 15:37:14 -0400

    3zzz

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ function App() {
   SwiperCore.use([Autoplay])
   return (
     <div className="App">
+    {/* <div className="App" className="fade-in"> */}
 
 
 
@@ -84,7 +85,7 @@ function App() {
         </h1> 
         <p> </p>
 
-        <p>Software Engineer with 4+ years of experience. I specialize in building web apps using React (Javascript/Typescript) front-ends and NodeJS back-ends. I also work with Python and Solidity.</p>
+        <p>Software Engineer with 3+ years of experience. I specialize in building web apps using React (Javascript/Typescript) front-ends and NodeJS back-ends. I also work with Python and Solidity.</p>
 
 <p> </p>
 <p>I work remotely with teams to build modern and reliable solutions to complex problems – helping global companies, start-ups and web agencies reach business goals with technology, on-budget, on-time. I am currently in Toronto, Canada.</p>
